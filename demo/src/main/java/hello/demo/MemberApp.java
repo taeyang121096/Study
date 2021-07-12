d091ce1e481b003e03fd585716c05edbe028d449
taeyang121096 <taeyang121096@naver.com>
2021-07-13 00:13:59 +0900

    spring basic

---
+++ demo/src/main/java/hello/demo/MemberApp.java
@@ -0,0 +1,21 @@
+package hello.demo;
+
+import hello.demo.member.Grade;
+import hello.demo.member.Member;
+import hello.demo.member.MemberService;
+import hello.demo.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("find Member = " + findMember.getName());
+        System.out.println("new member = " + member.getName());
+
+
+
+    }
+}
