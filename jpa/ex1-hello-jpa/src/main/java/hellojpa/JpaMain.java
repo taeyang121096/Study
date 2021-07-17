ee8d4e677c0df24d58dc89efeb4b762f45349fca
taeyang121096 <taeyang121096@naver.com>
2021-07-17 19:08:50 +0900

    jpa principle whit entitycontainer and entitymanager

---
+++ jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,33 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        //code
+        try{
+//            Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+
+            tx.commit();
+
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
