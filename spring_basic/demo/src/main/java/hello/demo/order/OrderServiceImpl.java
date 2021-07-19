32d9bd7b3de9283dcb675684793c29a6d449553c
taeyang121096 <taeyang121096@naver.com>
2021-07-19 20:33:30 +0900

    spring component scan and auto injection

--- spring_basic/demo/src/main/java/hello/demo/order/OrderServiceImpl.java
+++ spring_basic/demo/src/main/java/hello/demo/order/OrderServiceImpl.java
@@ -6,12 +6,15 @@ import hello.demo.discount.RateDiscountPolicy;
 import hello.demo.member.Member;
 import hello.demo.member.MemberRepository;
 import hello.demo.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
---
+++ spring_basic/demo/src/test/java/hello/demo/scan/AutoAppConfigTest.java
@@ -0,0 +1,23 @@
+package hello.demo.scan;
+
+import hello.demo.AutoAppConfig;
+import hello.demo.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+
+        assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+}
