1f1241b7ba8078a8ae5ac5c539d40475a9c63187
taeyang121096 <taeyang121096@naver.com>
2021-08-08 19:41:59 +0900

    entitiy class dev

--- spring_application/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ spring_application/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,15 +1,13 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
