57d6d87218432db638c30dd670e0b6af8c746987
taeyang121096 <taeyang121096@naver.com>
2021-08-21 20:48:30 +0900

    dev about api member update

---
+++ spring_application/jpashop/src/main/java/jpabook/jpashop/InitDb.java
@@ -0,0 +1,9 @@
+package jpabook.jpashop;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+}
--- spring_application/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ spring_application/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -44,4 +44,11 @@ public class MemberService {
         return memberRepository.findOne(memberId);
     }
 
+    @Transactional
+    public void update(Long id, String name){
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+
+    }
+
 }
