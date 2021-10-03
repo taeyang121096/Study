2795ba0ce4338f75980bc9ada3443878c09787e1
taeyang121096 <taeyang121096@naver.com>
2021-10-04 00:12:49 +0900

    springmvc validation about http and message

--- spring_MVC/springmvc/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ spring_MVC/springmvc/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,25 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
 
     private Long id;
+//    @NotBlank(groups = UpdateCheck.class)
     private String itemName;
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min =1000,max=1000000,groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999,groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
---
+++ spring_MVC/springmvc/validation/src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -0,0 +1,27 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/validation/api/items")
+public class ValidationItemApiController {
+
+    @PostMapping("/add")
+    public Object addItem(@RequestBody @Validated ItemSaveForm form, BindingResult bindingResult){
+        log.info("Api controller");
+        if(bindingResult.hasErrors()){
+            log.info("errors={}",bindingResult);
+            return bindingResult.getAllErrors();
+        }
+        log.info("success");
+        return form;
+    }
+}
